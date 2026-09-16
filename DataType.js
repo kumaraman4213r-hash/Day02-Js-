4b57990092fde011c5dd7ded3c89af1a748dd739
Kumar Aman <kumaraman4213r@gmail.com>
2026-09-16 22:14:40 +0530

    initial Day02(Js) commit

---
+++ DataType.js
@@ -0,0 +1,61 @@
+// dataType
+//  * primitive data type :-
+      // number ,string ,boolean , undrfined , null ,bigint, symbol
+
+      // number
+      let a = 10
+      let b = 2.36
+      console.log(a+b)
+
+      // string  
+      let c = "this is string\n"
+      let d = "this is java script"
+      console.log(c,d)
+
+      //boolean 
+      let login = true
+      let f = false
+      console.log(login,f)
+
+      //undefined
+
+      let user
+      console.log(user)
+
+      //bigint
+      let num = 36774646766376474n
+      console.log(num)
+
+      // null
+      let weather  = null
+      console.log(weather)
+
+      //symbol
+      const id1 = Symbol("id")
+      console.log(id1) 
+
+     
+
+
+
+
+//  * Non primitive DataType
+       // Arrays, object ,function       
+
+       //array
+       let arr = [10,29,11,36,"Aman",true]
+       console.log(arr)
+
+       //object
+
+       let person = {
+        name: "Aman",
+        age: 20,
+        category:getSelection
+        }
+        
+       
+
+        
+       
+
---
+++ index.js
@@ -0,0 +1,20 @@
+//let key allow to change  value and same variable not declare multiplr time
+let name = "Kumar Aman"
+let age = 20
+age = 30
+
+console.log(name,age)
+
+// const not allow to change 
+
+const acc = 2020
+//acc = 1234
+console.log(acc) 
+
+
+//old method  same variable declare multiple time and not gives error
+var  a =  10
+     a =  30
+var  a =  44
+
+console.log(a)
