7279520fba87917f6b5f0e54e7109cfb59d8ee64
Kumar Aman <kumaraman4213r@gmail.com>
2026-09-18 10:21:11 +0530

    updated code

--- DataType.js
+++ DataType.js
@@ -32,17 +32,31 @@
 
       //symbol
       const id1 = Symbol("id")
-      console.log(id1) 
+      console.log(id1)
+       console.log(typeof id1)
 
-     
+
+     let str = "Aman"
+         str = "kuamr"
+         console.log(str[0])  //  this is not example of  immutable
 
 
+         let xyz  = "msdhoni"
+          xyz[0] = "v"
+          console.log(xyz)  // primitive data type aree immutable 
+
+// pass by value
+let x = 10
+let y = x
+y = 20
+console.log(x,y)  //not mutable
 
 
 //  * Non primitive DataType
        // Arrays, object ,function       
 
        //array
+
        let arr = [10,29,11,36,"Aman",true]
        console.log(arr)
 
@@ -51,8 +65,30 @@
        let person = {
         name: "Aman",
         age: 20,
-        category:getSelection
+        category:'gen'
         }
+
+        // function
+         let s = function add(){
+            console.log("hello");
+        }
+        console.log(s)
+
+        //non primitive data type are mutable
+        let arr1 = [10,20,30,40]
+        arr1.push(90)
+        arr1[0] = 100 //mutate
+        console.log(arr1)
+
+
+        let  obj ={
+            name: "Aman",
+            age: 20
+
+        }
+        obj.name = "kumar"
+        console.log(obj)
+        
         
        
 
